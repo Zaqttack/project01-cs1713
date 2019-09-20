0d276aebf0f16dc1c34e8e10f75564901d297a09
George Machine <zaquariah@gmail.com>
2019-09-19 21:51:15 -0500

    initial commit

---
+++ airport-program.h
@@ -0,0 +1,17 @@
+/*
+    airport-program.h
+    Project 1
+    Firstname Lastname
+
+    This file is the header file for declaring the functions used in the 'airport-program.c' file.
+    It is also used for including the 'airport.h' header file where the constants, airport structure and other functions are declared.
+*/
+#include "airport.h"
+
+/* 
+    printMenu
+    ---------------------------
+    This function prints the main menu.
+    Returns: Nothing
+*/
+void printMenu();
